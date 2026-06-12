3b68701a2db834b7eaba29682840a284ecbd1149
xiue233 <xiue233@qq.com>
2026-06-12 21:28:34 +0800

    feat: add demo app

---
+++ Demo/SweetEditorDemo/SweetEditorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetEditorDemoApp.swift
+//  SweetEditorDemo
+//
+//  Created by xiue233 on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct SweetEditorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo/SweetEditorDemoTests/SweetEditorDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SweetEditorDemoTests.swift
+//  SweetEditorDemoTests
+//
+//  Created by xiue233 on 2026/5/29.
+//
+
+import Testing
+@testable import SweetEditorDemo
+
+struct SweetEditorDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
